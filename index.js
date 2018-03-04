e62dc78a36e7b8e9b6badaf71d882436c5bba1d4
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-03-04 20:58:09 +0200

    connected to mongolab

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require('express')
 const app = express()
 const config = require('./utils/config')
+const mongoose = require('mongoose')
+const http = require('http')
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
 })
 
+mongoose.connect(config.MONGOLAB_URL)
+mongoose.Promise = global.Promise
+
 const PORT = config.PORT
 
-app.listen(PORT, () => {
+const server = http.createServer(app)
+
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+server.on('close', () => {
+  mongoose.connection.close()
+})
+
+module.exports = {
+  app,server
+}
