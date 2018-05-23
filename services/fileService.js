8c16123ad2f3a511c169ad633c3b100e0a9509b9
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-05-23 14:17:33 +0300

    progressing in file parsing

--- services/fileService.js
+++ services/fileService.js
@@ -5,7 +5,7 @@ const convertBase64ToExcel = (base64String) => {
   const withoutPrefix = base64String.substring(37)
   const options = { type: 'base64' }
   const xlsFromBase64 = xlsx.read(withoutPrefix, options)
-  console.log('WORKBOOK ', xlsFromBase64)
+  return xlsx.utils.sheet_to_json(xlsFromBase64.Sheets['Rating järjestys'])
 }
 
 module.exports = { convertBase64ToExcel }
---
+++ services/rankingService.js
@@ -0,0 +1,37 @@
+
+const convertColumnToRankingObject = (column) => {
+  const nameString= 'Pelaajalla pitää olla vähintään yksi kisatulos (Kevät-18 tai Syksy-17) jotta näkyisi tällä listalla'
+  const positionString = '__EMPTY'
+  const clubString = '__EMPTY_3'
+  const ratingString = '__EMPTY_4'
+  return {
+    name: column[nameString],
+    position: column[positionString],
+    club: column[clubString],
+    rating: column[ratingString]
+  }
+}
+
+const saveRankingToDatabase = (rankingJson) => {
+  const nameString= 'Pelaajalla pitää olla vähintään yksi kisatulos (Kevät-18 tai Syksy-17) jotta näkyisi tällä listalla'
+  const noMorePlayers = 'Seuraavilla pelaajilla on rating mutta ei yhtään kisatulosta (Kevät-18 tai Syksy-17) eli eivät mukana ylläolevalla listalla'
+  rankingJson.every(function(element, index)  {
+    if (index > 2) {
+      if (element[nameString] === noMorePlayers) {
+        console.log('RANKING ENDED')
+        return false
+      }
+      const object = convertColumnToRankingObject(element)
+      console.log(object)
+      return true
+    }
+    return true
+  })
+  /*
+  for (let i = 2; i < 103; i++) {
+    const object = convertColumnToRankingObject(rankingJson[i])
+    console.log('OBJECT', object)
+  }*/
+}
+
+module.exports = { saveRankingToDatabase }
