e72c600ef52be2922a338a1938b353bf68fe1265
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-05-27 14:32:42 +0300

    logint ested

--- e2e/login.test.js
+++ e2e/login.test.js
@@ -1,4 +1,16 @@
 const puppeteer = require('puppeteer')
+const User = require('../models/user')
+const seeder = require('../db/seeds')
+const config = require('../utils/config')
+const mongoose = require('mongoose')
+
+beforeAll(async () => {
+  console.log('CONNECTING to ',config.MONGOLAB_URL)
+  mongoose.connect(config.MONGOLAB_URL)
+  mongoose.Promise = global.Promise
+  await User.remove({})
+  await seeder.seedAdminToDataBase()
+})
 
 describe('When user goest to login page ', async() => {
   let browser
@@ -15,7 +27,35 @@ describe('When user goest to login page ', async() => {
     expect(includes).toBe(true)
   })
 
+  test(' and fills wrong credentials, login fails', async () => {
+    await login(process.env.ADMIN_USERNAME, 'wrongPassword')
+    await page.waitForSelector('.success')
+    const textContent = await page.$eval('body', el => el.textContent)
+    const includes = textContent.includes('Wrong username or password!')
+    expect(includes).toBe(true)
+  },10000)
+
+  test(' and fills correct credentials, login succeeds', async () => {
+    await login(process.env.ADMIN_USERNAME, process.env.ADMIN_PASSWORD)
+    await page.waitForSelector('.success')
+    const textContent = await page.$eval('body', el => el.textContent)
+    const includes = textContent.includes('Welcome back Admin!')
+    expect(includes).toBe(true)
+  },10000)
+
+  const login = async(username, password) => {
+    await page.type('input', username)
+    await page.type('input[type=password]', password)
+    await page.click('button')
+  }
+
   afterEach(async () => {
     await browser.close()
   })
+
+})
+
+afterAll( async () => {
+  await User.remove({})
+  mongoose.connection.close()
 })
