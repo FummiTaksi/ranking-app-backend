e7535c493dec0b47abf110cafe32c791b5709e40
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-06-07 21:28:12 +0300

    testing valid rankingName

--- tests/api/ranking_api.test.js
+++ tests/api/ranking_api.test.js
@@ -2,19 +2,41 @@ const supertest = require('supertest')
 const { app, server } = require('../../index')
 const api = supertest(app)
 
-describe('/api/ranking ', () => {
+describe('/api/ranking', () => {
   describe('/new', () => {
-    test(' returns 400 if token is not correct', async() => {
-      const wrongCredentials = {
-        file: undefined,
-        name: 'Test Rank'
-      }
-      await api
+    describe(' returns 400 when ', () => {
+
+
+      test(' token is not correct', async() => {
+        const wrongCredentials = {
+          file: undefined,
+          rankingName: 'Test Rank',
+          rankingDate: new Date()
+        }
+        await api
+          .post('/api/ranking/new')
+          .send(wrongCredentials)
+          .set('Authorization', 'bearer xdlsdjuna')
+          .expect(400)
+      },10000)
+      test(' body is missing rankingName', async() => {
+        const correctCredentials = {
+          username: process.env.ADMIN_USERNAME,
+          password: process.env.ADMIN_PASSWORD
+        }
+        const response = await api.post('/api/login').send(correctCredentials)
+        const missingRankingName = {
+          file: 'asfasa',
+          rankingDate: new Date()
+        }
+        await api
         .post('/api/ranking/new')
-        .send(wrongCredentials)
-        .set('Authorization', 'bearer xdlsdjuna')
+        .send(missingRankingName)
+        .set('Authorization', 'bearer ' + response.body.token)
         .expect(400)
-    },10000)
+      },10000)
+    })
+
   })
 })
 
