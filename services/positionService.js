4ad013d1814261825b60c796a1a28d4b3e8b3f46
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-06-10 21:04:37 +0300

    implementing createPosition

---
+++ services/positionService.js
@@ -0,0 +1,9 @@
+const Position = require('../models/position')
+
+const createPosition = async(positionBody) => {
+  const positionModel = new Position(positionBody)
+  const response = await positionModel.save()
+  return response
+}
+
+module.exports = { createPosition }
