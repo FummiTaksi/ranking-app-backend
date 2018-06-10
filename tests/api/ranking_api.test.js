75577ff2c743a074f1c156a2a8f3abba21de0a7c
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-06-10 19:49:47 +0300

    using getCorrectToken helper method

--- tests/api/ranking_api.test.js
+++ tests/api/ranking_api.test.js
@@ -2,6 +2,15 @@ const supertest = require('supertest')
 const { app, server } = require('../../index')
 const api = supertest(app)
 
+const getCorrectToken = async() => {
+  const correctCredentials = {
+    username: process.env.ADMIN_USERNAME,
+    password: process.env.ADMIN_PASSWORD
+  }
+  const response = await api.post('/api/login').send(correctCredentials)
+  return response.body.token
+}
+
 describe('/api/ranking', () => {
   describe('/new', () => {
     describe(' returns 400 when ', () => {
@@ -21,36 +30,28 @@ describe('/api/ranking', () => {
         expect(response.body.error).toEqual('You must be signed in admin to create new ranking!');
       },10000)
       test(' body is missing rankingName', async() => {
-        const correctCredentials = {
-          username: process.env.ADMIN_USERNAME,
-          password: process.env.ADMIN_PASSWORD
-        }
-        const response = await api.post('/api/login').send(correctCredentials)
         const missingRankingName = {
           file: 'asfasa',
           rankingDate: new Date()
         }
+        const token = await getCorrectToken()
         const rankingResponse = await api
         .post('/api/ranking/new')
         .send(missingRankingName)
-        .set('Authorization', 'bearer ' + response.body.token)
+        .set('Authorization', 'bearer ' + token)
         .expect(400)
         expect(rankingResponse.body.error).toEqual('Ranking must have a name!')
       },10000)
       test(' body is missing rankingDate', async() => {
-        const correctCredentials = {
-          username: process.env.ADMIN_USERNAME,
-          password: process.env.ADMIN_PASSWORD
-        }
-        const response = await api.post('/api/login').send(correctCredentials)
         const missingRankingDate= {
           file: 'asfasa',
           rankingName: 'Test Cup'
         }
+        const token = await getCorrectToken()
         const rankingResponse = await api
         .post('/api/ranking/new')
         .send(missingRankingDate)
-        .set('Authorization', 'bearer ' + response.body.token)
+        .set('Authorization', 'bearer ' + token)
         .expect(400)
         expect(rankingResponse.body.error).toEqual('Ranking must have a date!')
       },10000)
