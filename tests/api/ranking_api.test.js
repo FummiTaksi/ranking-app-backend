de68c9c73d9f4cd10e048006323fa856c68ac47e
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-06-10 19:45:21 +0300

    testing that all unvalid requests return 400

--- tests/api/ranking_api.test.js
+++ tests/api/ranking_api.test.js
@@ -13,11 +13,12 @@ describe('/api/ranking', () => {
           rankingName: 'Test Rank',
           rankingDate: new Date()
         }
-        await api
+        const response = await api
           .post('/api/ranking/new')
           .send(wrongCredentials)
           .set('Authorization', 'bearer xdlsdjuna')
           .expect(400)
+        expect(response.body.error).toEqual('You must be signed in admin to create new ranking!');
       },10000)
       test(' body is missing rankingName', async() => {
         const correctCredentials = {
@@ -29,11 +30,29 @@ describe('/api/ranking', () => {
           file: 'asfasa',
           rankingDate: new Date()
         }
-        await api
+        const rankingResponse = await api
         .post('/api/ranking/new')
         .send(missingRankingName)
         .set('Authorization', 'bearer ' + response.body.token)
         .expect(400)
+        expect(rankingResponse.body.error).toEqual('Ranking must have a name!')
+      },10000)
+      test(' body is missing rankingDate', async() => {
+        const correctCredentials = {
+          username: process.env.ADMIN_USERNAME,
+          password: process.env.ADMIN_PASSWORD
+        }
+        const response = await api.post('/api/login').send(correctCredentials)
+        const missingRankingDate= {
+          file: 'asfasa',
+          rankingName: 'Test Cup'
+        }
+        const rankingResponse = await api
+        .post('/api/ranking/new')
+        .send(missingRankingDate)
+        .set('Authorization', 'bearer ' + response.body.token)
+        .expect(400)
+        expect(rankingResponse.body.error).toEqual('Ranking must have a date!')
       },10000)
     })
 
