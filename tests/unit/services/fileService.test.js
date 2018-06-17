56cfe194802e44db9633fbe3b72772eac9874485
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-06-17 18:17:51 +0300

    jsons length is 11 because testfile changed

--- tests/unit/services/fileService.test.js
+++ tests/unit/services/fileService.test.js
@@ -5,6 +5,6 @@ describe.only('FileService ', () => {
   test(' converts base64 to json without ', () => {
     const base64 = testHelpers.getRatingBase64()
     const fileJson = fileService.convertBase64ToExcel(base64)
-    expect(fileJson.length).toEqual(10)
+    expect(fileJson.length).toEqual(11)
   })
 })
