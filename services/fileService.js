e8ce96813787ca827823821af2cf2952cf7ab951
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-06-17 12:28:32 +0300

    remove debug print

--- services/fileService.js
+++ services/fileService.js
@@ -2,7 +2,6 @@ const xlsx = require('xlsx')
 
 const convertBase64ToExcel = (base64String) => {
   const withoutPrefix = base64String.substring(37)
-  console.log('withoutPrefix', withoutPrefix)
   const options = { type: 'base64' }
   const xlsFromBase64 = xlsx.read(withoutPrefix, options)
   return xlsx.utils.sheet_to_json(xlsFromBase64.Sheets['Rating järjestys'])
