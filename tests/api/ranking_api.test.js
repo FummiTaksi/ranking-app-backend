a66fd5f5d55ecb2f46f7fe5b90e5d614c9741dc2
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-06-24 21:55:55 +0300

    test successfull post for ranking and get method of rankings

--- tests/api/ranking_api.test.js
+++ tests/api/ranking_api.test.js
@@ -2,14 +2,23 @@ const supertest = require('supertest')
 const { app, server } = require('../../index')
 const api = supertest(app)
 const User = require('../../models/user')
+const Ranking = require('../../models/ranking')
+const Position = require('../../models/position')
 const seeder = require('../../db/seeds')
+const testHelpers = require('../helpers/testHelpers')
 
 
 beforeAll(async() => {
-  await User.remove({})
+  await emptyDatabase()
   await seeder.seedAdminToDataBase()
-  console.log('SEEDED IN HELLO WORLD')
+  console.log('beforeAll in ranking api')
 })
+
+const emptyDatabase = async() => {
+  await User.remove({})
+  await Ranking.remove({})
+  await Position.remove({})
+}
 
 const getCorrectToken = async() => {
   const correctCredentials = {
@@ -20,58 +29,91 @@ const getCorrectToken = async() => {
   return response.body.token
 }
 
+const correctCredentials = () => {
+  const base64 = testHelpers.getRatingBase64()
+  return {
+    rankingFileBase64: base64,
+    rankingName: 'Test Cup',
+    rankingDate: new Date()
+  }
+}
+
+const postNewRanking = async(credentials, token) => {
+  const rankingResponse = await api
+    .post('/api/ranking/new')
+    .send(credentials)
+    .set('Authorization', 'bearer ' + token)
+  return rankingResponse
+}
 
 describe('/api/ranking', () => {
   describe('/new', () => {
     describe(' returns 400 when ', () => {
 
       test(' token is not correct', async() => {
-        const wrongCredentials = {
-          file: undefined,
-          rankingName: 'Test Rank',
-          rankingDate: new Date()
-        }
-        const response = await api
-          .post('/api/ranking/new')
-          .send(wrongCredentials)
-          .set('Authorization', 'bearer xdlsdjuna')
-          .expect(400)
-        expect(response.body.error).toEqual('You must be signed in admin to create new ranking!');
+        const response = await postNewRanking(correctCredentials(), 'bearer wrongtoken')
+        expect(response.status).toEqual(400)
+        expect(response.body.error).toEqual('You must be signed in admin to create new ranking!')
       },10000)
       test(' body is missing rankingName', async() => {
-        const missingRankingName = {
-          file: 'asfasa',
-          rankingDate: new Date()
-        }
+        const missingRankingName = correctCredentials()
+        missingRankingName.rankingName = undefined
         const token = await getCorrectToken()
-        const rankingResponse = await api
-        .post('/api/ranking/new')
-        .send(missingRankingName)
-        .set('Authorization', 'bearer ' + token)
-        .expect(400)
+        const rankingResponse = await postNewRanking(missingRankingName, token)
+        expect(rankingResponse.status).toEqual(400)
         expect(rankingResponse.body.error).toEqual('Ranking must have a name!')
+        console.log('STATUS', rankingResponse.status)
       },10000)
       test(' body is missing rankingDate', async() => {
-        const missingRankingDate= {
-          file: 'asfasa',
-          rankingName: 'Test Cup'
-        }
+        const missingRankingDate = correctCredentials()
+        missingRankingDate.rankingDate = undefined
         const token = await getCorrectToken()
-        const rankingResponse = await api
-        .post('/api/ranking/new')
-        .send(missingRankingDate)
-        .set('Authorization', 'bearer ' + token)
-        .expect(400)
+        const rankingResponse = await postNewRanking(missingRankingDate, token)
+        expect(rankingResponse.status).toEqual(400)
         expect(rankingResponse.body.error).toEqual('Ranking must have a date!')
       },10000)
     })
 
+    describe(' returns 200 when ', () => {
+
+      test(' body contains correct credentials', async() => {
+        const token = await getCorrectToken()
+        const response = await postNewRanking(correctCredentials(), token)
+        expect(response.status).toEqual(200)
+        expect(response.body.ranking.competitionName).toEqual('Test Cup')
+      },10000)
+
+    })
+
+  })
+
+  describe(' GET /', () => {
+
+    beforeAll(async() => {
+      await Ranking.remove({})
+      await Position.remove({})
+      console.log('beforeAll in ranking api')
+    })
+    test(' returns status 200 and correct amount of rankings ', async() => {
+      /*await emptyDatabase()
+      await seeder.seedAdminToDataBase()*/
+      const token = await getCorrectToken()
+      await postNewRanking(correctCredentials(), token)
+      const response = await api.get('/api/ranking/').expect(200)
+      expect(response.body.rankings.length).toEqual(1)
+    })
+
+    afterAll(async() => {
+      await Ranking.remove({})
+      await Position.remove({})
+      console.log('beforeAll in ranking api')
+    })
   })
 })
 
 
 afterAll(async() => {
-  await User.remove({})
+  await emptyDatabase()
   server.close()
   console.log('RANKING END')
 })
