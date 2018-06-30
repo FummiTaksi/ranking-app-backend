1e612577e8252978275758b714a0037e311ec507
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-06-30 13:02:24 +0300

    remove rankings positions when deleting ranking

--- models/ranking.js
+++ models/ranking.js
@@ -12,6 +12,10 @@ const rankingSchema = new mongoose.Schema({
   positions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Position' }]
 })
 
+rankingSchema.pre('remove', async function(next) {
+  await this.model('Position').remove({ ranking: this._id }, next)
+})
+
 const Ranking = mongoose.model('Ranking', rankingSchema)
 
 module.exports = Ranking
