05f32e5e25d45ee5a6b9b41f230baba15e1d89e7
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-07-08 19:25:27 +0300

    test that uploading is accessable only for admin

--- tests/e2e/upload.test.js
+++ tests/e2e/upload.test.js
@@ -27,7 +27,7 @@ describe('When user goes to upload page ', () => {
     await page.goto('http://localhost:3003/#/signin')
   })
 
-  describe.only(' and is signed in', () => {
+  describe(' and is signed in', () => {
 
     beforeEach(async () => {
       await Ranking.remove({})
@@ -47,27 +47,51 @@ describe('When user goes to upload page ', () => {
       expect(includes).toBeTruthy()
     },10000)
 
-    test.only(' ranking can be deleted', async () => {
+    test(' ranking can be deleted', async () => {
       await login(page, process.env.ADMIN_USERNAME, process.env.ADMIN_PASSWORD)
       await uploadRanking(page)
       await page.goto('http://localhost:3003/#/rankings')
-      await page.waitForSelector('button')
-      await page.click('button')
+      await page.waitForSelector('.delete')
+      await page.click('.delete')
+      await page.waitFor(5000)
       await page.waitForSelector('p')
       const textContent = await page.$eval('body', el => el.textContent)
-      console.log('textcontent', textContent)
-      const includes = textContent.includes('Here are all 1 rankings that are uploaded to this site')
+      const includes = textContent.includes('No rankings saved to database yet')
       expect(includes).toBeTruthy()
     },10000)
 
   })
  
-  test(' and is not signed in, loading files is not possible', async () => {
-    await page.goto('http://localhost:3003/#/upload')
-    const textContent = await page.$eval('body', el => el.textContent)
-    const includes = textContent.includes('excel')
-    expect(includes).toBeFalsy()
-  },10000)
+  describe(' and is not signed in', () => {
+
+    beforeEach(async () => {
+      await Ranking.remove({})
+      await Position.remove({})
+      browser = await puppeteer.launch({ args: ['--no-sandbox'] })
+      page = await browser.newPage()
+      await page.goto('http://localhost:3003/#/signin')
+    })
+
+    test(' loading files is not possible', async () => {
+      await page.goto('http://localhost:3003/#/upload')
+      const textContent = await page.$eval('body', el => el.textContent)
+      const includes = textContent.includes('excel')
+      expect(includes).toBeFalsy()
+    },10000)
+
+    test('deleting rankings is not possible', async() => {
+      await login(page, process.env.ADMIN_USERNAME, process.env.ADMIN_PASSWORD)
+      await uploadRanking(page)
+      await page.click('button')
+      await page.goto('http://localhost:3003/#/rankings')
+      await page.waitFor(3000)
+      await page.waitForSelector('p')
+      const textContent = await page.$eval('body', el => el.textContent)
+      const includes = textContent.includes('Delete')
+      expect(includes).toBeFalsy()
+    },10000)
+  })
+
 
   afterEach(async () => {
     await browser.close()
