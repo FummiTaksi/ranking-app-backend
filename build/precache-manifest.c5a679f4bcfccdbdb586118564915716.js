7dbd118f9c400acd825cb54805fcee978c8717a8
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2019-01-18 14:47:11 +0200

    frontend date fix

---
+++ build/precache-manifest.c5a679f4bcfccdbdb586118564915716.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ff5a7f778a582b4b8c44",
+    "url": "/static/js/main.ff5a7f77.chunk.js"
+  },
+  {
+    "revision": "60e2ffe824d01fe7a76a",
+    "url": "/static/js/1.60e2ffe8.chunk.js"
+  },
+  {
+    "revision": "aed999abd7bc4fd3529d95f9593b532d",
+    "url": "/index.html"
+  }
+];
