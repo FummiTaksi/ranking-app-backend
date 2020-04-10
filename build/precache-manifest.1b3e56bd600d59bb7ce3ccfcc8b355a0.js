4c1cae49c04dc7051883d721f4cec0269c55f230
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2020-04-10 14:00:04 +0300

    update frontend packages

---
+++ build/precache-manifest.1b3e56bd600d59bb7ce3ccfcc8b355a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2a86bc19405f678e9306f0c6035b593",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc3f6332c884457b695d",
+    "url": "/static/js/2.cf717887.chunk.js"
+  },
+  {
+    "revision": "2315edd2fdb1cb76bd52b3819d4335a2",
+    "url": "/static/js/2.cf717887.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9d7b2599add468e803c",
+    "url": "/static/js/main.d4581cba.chunk.js"
+  },
+  {
+    "revision": "0fef5950e3eed3fe783d",
+    "url": "/static/js/runtime-main.d009ff45.js"
+  }
+]);
